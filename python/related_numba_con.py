fe643de9af6f01d875eda01ebe76327f291fcc92
Ivan <fizmat.r66@gmail.com>
2023-10-05 20:12:31 +0400

    try: replace tag-to-post list of arrays with 2d array too

--- python/related_numba_con.py
+++ python/related_numba_con.py
@@ -3,18 +3,18 @@ from timing import lap, finish
 lap()
 import numpy as np
 import orjson
-from numba import njit, prange, typed
+from numba import njit, prange
 
 
 @njit(parallel=True)
-def get_all_top5(n_posts, t_to_pp, p_to_tt, p_to_nt):
+def get_all_top5(n_posts, t_to_pp, t_to_np, p_to_tt, p_to_nt):
     all_top5 = np.empty((n_posts, 5), np.uint16)
     for current_p in prange(n_posts):
         tt = p_to_tt[current_p]
         relation_count = np.zeros(n_posts, dtype=np.uint8)
         for tj in range(p_to_nt[current_p]):
-            for p in t_to_pp[tt[tj]]:
-                relation_count[p] += 1
+            for pi in range(t_to_np[tt[tj]]):
+                relation_count[t_to_pp[tt[tj], pi]] += 1
         relation_count[current_p] = 0
 
         # get top 5:
@@ -40,7 +40,8 @@ def precompile():
     # 2) get correct processing (without compilation) time using the cached machine code
     get_all_top5(
         0,
-        typed.List([np.empty(0, dtype=np.uint16)]),
+        np.empty((0, 0), dtype=np.uint16),
+        np.empty(0, dtype=np.uint16),
         np.empty((0, 0), dtype=np.uint8),
         np.empty(0, dtype=np.uint8),
     )
@@ -55,17 +56,21 @@ def main():
 
     unique_tags = set(tag for post in posts for tag in post["tags"])
     tag_to_t = {t: np.uint8(i) for i, t in enumerate(unique_tags)}
-    t_to_pp = [[] for _ in unique_tags]
+    t_to_pp_list = [[] for _ in unique_tags]
     p_to_nt = np.array([len(post["tags"]) for post in posts], dtype=np.uint8)
     p_to_tt = np.empty((len(posts), p_to_nt.max()), dtype=np.uint8)
 
     for p, post in enumerate(posts):
         for tj, tag in enumerate(post["tags"]):
-            t_to_pp[tag_to_t[tag]].append(p)
+            t_to_pp_list[tag_to_t[tag]].append(p)
             p_to_tt[p, tj] = tag_to_t[tag]
-    t_to_pp = typed.List(np.array(tp, dtype=np.uint16) for tp in t_to_pp)
+    t_to_np = np.empty(len(unique_tags), dtype=np.uint16)
+    t_to_pp = np.empty((len(unique_tags), max(len(posts) for posts in t_to_pp_list)), dtype=np.uint16)
+    for t, pp in enumerate(t_to_pp_list):
+        t_to_np[t] = len(pp)
+        t_to_pp[t, :len(pp)] = pp
 
-    all_top5 = get_all_top5(len(posts), t_to_pp, p_to_tt, p_to_nt)
+    all_top5 = get_all_top5(len(posts), t_to_pp, t_to_np, p_to_tt, p_to_nt)
 
     all_related_posts = []
     for post, top5 in zip(posts, all_top5):
